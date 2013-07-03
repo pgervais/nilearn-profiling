80b084e4280607b6d9b944c36692e99fdf151349
Philippe Gervais <philippe.gervais@inria.fr>
2013-07-03 13:40:48 +0200

    Adapted prof_group_sparse_covariance.py
    The group sparse function signatures have changed. Example has been
    adapted and improved.

--- prof_group_sparse_covariance.py
+++ prof_group_sparse_covariance.py
@@ -3,173 +3,26 @@ precision matrices."""
 
 # Authors: Philippe Gervais
 # License: simplified BSD
-import numpy as np
-from scipy import linalg
 
 from nilearn.group_sparse_covariance import group_sparse_covariance
+from nilearn.tests.test_group_sparse_covariance \
+        import generate_multi_task_gg_model
 import utils  # defines profile() if not already defined
 
 
-## def example():
-##     display = True
-
-##     signals, precisions, topology = generate_multi_task_gg_model(
-##         density=0.1, n_tasks=5, n_var=10, min_samples=100, max_samples=151,
-##         rand_gen=np.random.RandomState(0))
-##     emp_covs = [np.dot(s.T, s) / len(s) for s in signals]
-
-##     if display:
-##         import pylab as pl
-
-##     rho = 0.8
-##     n_samples = [len(signal) for signal in signals]
-
-##     omega, costs = group_sparse_covariance(emp_covs, rho, n_samples, n_iter=12,
-##                                            verbose=1, debug=False,
-##                                            return_costs=True)
-
-##     if display:
-
-##         for n, value in enumerate(zip(signals, precisions)):
-##             s, prec = value
-##             pl.figure()
-##             pl.subplot(1, 2, 1)
-##             pl.imshow(omega[..., n] != 0, interpolation="nearest", cmap="gray",
-##                       vmin=0, vmax=1)
-##             pl.colorbar()
-##             pl.title("sparsity {n:d}".format(n=n))
-
-##             pl.subplot(1, 2, 2)
-##             pl.imshow(omega[..., n], interpolation="nearest", cmap="gray")
-##             pl.colorbar()
-##             pl.title("precision {n:d}".format(n=n))
-
-##     if display:
-##         pl.figure()
-##         last = costs[-1]
-##         pl.loglog(np.asarray(costs - last
-##                              + 4 * (costs[-2] - costs[-1])))
-##         pl.ylabel("cost")
-##         pl.xlabel("iteration")
-
-##         pl.figure()
-##         pl.imshow(topology, interpolation="nearest", cmap="gray")
-##         pl.colorbar()
-##         pl.title("true sparsity")
-
-##     if display:
-##         pl.show()
-
-
-def generate_sparse_spd_matrix(n_var=1, alpha=0.95,
-                               rand_gen=np.random.RandomState(0),
-                               sparsity_pattern=None):
-    """
-    Generate a sparse symmetric definite positive matrix with the given
-    dimension.
-
-    sparsity_pattern: numpy.ndarray
-        only lower triangular half is used.
-
-    Returns
-    -------
-    prec: array of shape(n_var, n_var)
-    """
-    # Compute sparsity pattern
-    if sparsity_pattern is None:
-        aux = rand_gen.rand(n_var, n_var)
-        aux[aux < alpha] = 0
-        aux[aux >= alpha] = 1
-        aux = np.tril(aux, k=-1)
-        assert(np.all(np.triu(aux, k=1) == 0))
-        aux = aux + aux.T
-        # Permute the variables: we don't want to have any asymmetry in the
-        # sparsity matrix. aux must be symmetric
-        permutation = rand_gen.permutation(n_var)
-        aux = aux[permutation].T[permutation]
-        sparsity_pattern = aux
-        np.testing.assert_almost_equal(aux, aux.T)
-
-    # Compute sparse SDP matrix
-    aux = np.zeros((n_var, n_var))
-    # non-zero in sparsity_pattern means non-zero value in precision matrix
-    mask = sparsity_pattern != 0
-    aux[np.logical_not(mask)] = 0
-    aux[mask] = .9 * rand_gen.rand(np.sum(mask))
-    aux = np.tril(aux, k=-1)
-
-    chol = -np.eye(n_var)
-    chol += aux
-    prec = np.dot(chol.T, chol)
-    return prec, sparsity_pattern
-
-
-def generate_standard_sparse_mvn(n_tasks=5, n_samples=50, n_var=10, alpha=.95,
-                                 rand_gen=np.random.RandomState(0)):
-    """ Generate a multivariate normal samples with sparse precision, zero
-        mean and covariance diagonal equal to one.
-
-        Parameters
-        ----------
-        alpha: float
-            percentile of zero values in precision matrix. Values between 0 and
-            1. 0 gives a full matrix, 1 gives a identity precision matrix.
-
-        Returns
-        -------
-        x, array of shape (n_samples, n_var):
-            samples
-        prec, array of shape (n_var, n_var):
-            theoretical precision
-    """
-    precisions = []  # List of precision matrices
-    signals = []
-    sparsity_pattern = None
-    for n in xrange(n_tasks):
-        prec, sparsity_pattern = generate_sparse_spd_matrix(
-            n_var=n_var, alpha=alpha, rand_gen=rand_gen,
-            sparsity_pattern=sparsity_pattern)
-
-        # Inversion for SPD matrices
-        vals, vecs = linalg.eigh(prec)
-        cov = np.dot(vecs / vals, vecs.T)
-
-        # normalize covariance (and precision)
-        # in order to have unit values on the diagonal
-        idelta = np.diag(np.sqrt(np.diag(cov)))
-        delta = np.diag(1. / np.sqrt(np.diag(cov)))
-        cov = np.dot(np.dot(delta, cov), delta)
-        prec = np.dot(np.dot(idelta, prec), idelta)
-
-        # generate samples
-        x = rand_gen.multivariate_normal(mean=np.zeros(n_var), cov=cov,
-                                         size=(n_samples,))
-        signals.append(x)
-        precisions.append(prec)
-
-    return signals, precisions, sparsity_pattern
-
-
 def benchmark():
-    display = False
-    rho = 1.
-    signals, precisions, sparsity_pattern = generate_standard_sparse_mvn(
-        n_samples=150, n_var=70, n_tasks=40)
+    rho = .2
 
-    emp_covs = [np.dot(signal.T, signal) / signal.shape[0]
-                for signal in signals]
-    n_samples = [signal.shape[0] for signal in signals]
+    signals, _, _ = generate_multi_task_gg_model(n_tasks=40, n_var=30,
+                                              density=0.15,
+                                              min_samples=100, max_samples=150)
 
-    est_precs = utils.timeit(group_sparse_covariance)(emp_covs, rho,
-                                                      n_samples, n_iter=4,
-                                                      verbose=0, debug=False)
-    if display:
-        import pylab as pl
-        for n in xrange(min(est_precs.shape[-1], 5)):
-            pl.matshow(est_precs[..., n] != 0)
-        pl.matshow(sparsity_pattern + np.eye(sparsity_pattern.shape[0]))
-        pl.title("true sparsity pattern")
-        pl.show()
+    emp_covs, est_precs = utils.timeit(group_sparse_covariance)(
+        signals, rho, max_iter=20, tol=1e-5, verbose=0, debug=False)
+
+    ## import pylab as pl
+    ## pl.matshow(est_precs[..., 0])
+    ## pl.show()
 
 if __name__ == "__main__":
     benchmark()
