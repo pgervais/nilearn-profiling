9b2b5a3d06bb1c8aa85880a7a453e3374da0dbfb
Philippe Gervais <philippe.gervais@inria.fr>
2013-07-10 14:21:54 +0200

    Added test cases for groupe_sparse_covariance

--- prof_group_sparse_covariance.py
+++ prof_group_sparse_covariance.py
@@ -9,12 +9,15 @@ import os.path
 import numpy as np
 
 from nilearn.group_sparse_covariance import (group_sparse_covariance,
+                                             empirical_covariances,
                                              GroupSparseCovarianceCV)
 import nilearn.testing
 import utils  # defines profile() if not already defined
 
 
 def cache_array(arr, filename, decimal=7):
+    """Small caching function to check that some array has not changed
+    between two invocations."""
     assert filename.endswith(".npy")
     if os.path.isfile(filename):
         cached = np.load(filename)
@@ -80,5 +83,54 @@ def benchmark2():
     ## pl.matshow(est_precs[..., 0])
     ## pl.show()
 
+
+def lasso_gsc_comparison():
+    """Check that graph lasso and group-sparse covariance give the same
+    output for a single task."""
+    from sklearn.covariance import graph_lasso, empirical_covariance
+
+    parameters = {'n_tasks': 1, 'n_var': 20, 'density': 0.15,
+                  'rho': .2, 'tol': 1e-4, 'max_iter': 50}
+
+    signals, _, _ = generate_signals(parameters)
+
+    _, gsc_precision = utils.timeit(group_sparse_covariance)(
+        signals, parameters['rho'], max_iter=parameters['max_iter'],
+        tol=parameters['tol'], verbose=1, debug=False)
+
+    emp_cov = empirical_covariance(signals[0])
+    _, gl_precision = utils.timeit(graph_lasso)(
+        emp_cov, parameters['rho'], tol=parameters['tol'],
+        max_iter=parameters['max_iter'])
+
+    np.testing.assert_almost_equal(gl_precision, gsc_precision[..., 0],
+                                   decimal=3)
+
+
+def singular_cov_case():
+    """Check behaviour of algorithm for singular input matrix."""
+    parameters = {'n_tasks': 10, 'n_var': 40, 'density': 0.15,
+                  'rho': .1, 'tol': 1e-2, 'max_iter': 50,
+                  'min_samples': 10, 'max_samples': 15}
+
+    signals, _, _ = generate_signals(parameters)
+
+    emp_covs, _, _, _ = empirical_covariances(signals)
+
+    # Check that all covariance matrices are singular.
+    eps = np.finfo(float).eps
+    for k in range(emp_covs.shape[-1]):
+        eigvals = np.linalg.eigvalsh(emp_covs[..., k])
+        assert(abs(eigvals.min()) <= 50 * eps)
+
+    _, gsc_precisions = utils.timeit(group_sparse_covariance)(
+        signals, parameters['rho'], max_iter=parameters['max_iter'],
+        tol=parameters['tol'], verbose=1, debug=False)
+
+    print('found sparsity: {0:.3f}'
+          ''.format(1. * (gsc_precisions[..., 0] != 0).sum()
+                    / gsc_precisions.shape[0] ** 2))
+
 if __name__ == "__main__":
-    benchmark2()
+    ## lasso_gsc_comparison()
+    singular_cov_case()
